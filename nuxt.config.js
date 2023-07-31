b7ff8f61bd51de7051395b212f0de7466ed4c9a3
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2023-07-31 19:33:27 +0900

    feat: set router.base

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,10 +82,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  googleFonts: {
-    download: true,
-    families: {
-      Ubuntu: true,
-    },
+  router: {
+    base: '/portfolio/',
   },
 }
